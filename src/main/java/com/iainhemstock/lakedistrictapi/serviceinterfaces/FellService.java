8d740d7a4372d683504921fc9f22be2764e6dec1
Unknown <iainhemstock@googlemail.com>
2020-12-21 19:13:26 +0000

    added method to FellService interface

--- src/main/java/com/iainhemstock/lakedistrictapi/serviceinterfaces/FellService.java
+++ src/main/java/com/iainhemstock/lakedistrictapi/serviceinterfaces/FellService.java
@@ -1,8 +1,11 @@
 package com.iainhemstock.lakedistrictapi.serviceinterfaces;
 
 import com.iainhemstock.lakedistrictapi.domain.OsMapRef;
+import com.iainhemstock.lakedistrictapi.dtos.PagedCollectionDTO;
+import com.iainhemstock.lakedistrictapi.dtos.SummarisedFellDTO;
 import com.iainhemstock.lakedistrictapi.entities.Fell;
 
 public interface FellService {
     Fell getById(final OsMapRef osMapRef);
+    PagedCollectionDTO<SummarisedFellDTO> getSummarisedFells(final Integer offset, final Integer limit);
 }
