0c76ea19205d4f2b344907235567391128b544ce
Unknown <iainhemstock@googlemail.com>
2020-12-21 18:47:45 +0000

    renamed ModelMapperDetailedFellDTOAssemblerTest to ModelMapperFellDTOAssemblerTest

--- src/test/unit/java/com/iainhemstock/lakedistrictapi/assemblers/ModelMapperFellDTOAssemblerTest.java
+++ src/test/unit/java/com/iainhemstock/lakedistrictapi/assemblers/ModelMapperFellDTOAssemblerTest.java
@@ -25,7 +25,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertTrue;
 
-public class ModelMapperDetailedFellDTOAssemblerTest {
+public class ModelMapperFellDTOAssemblerTest {
 
 
     private FellDTOAssembler fellDTOAssembler;
