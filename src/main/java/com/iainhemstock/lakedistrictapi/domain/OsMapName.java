10c9e803d62b1d335d9e1c75fde1e6e08008e1b2
Unknown <iainhemstock@googlemail.com>
2020-12-22 19:34:23 +0000

    create OsMapName class

---
+++ src/main/java/com/iainhemstock/lakedistrictapi/domain/OsMapName.java
@@ -0,0 +1,14 @@
+package com.iainhemstock.lakedistrictapi.domain;
+
+public class OsMapName {
+    private final String value;
+
+    public OsMapName(final String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
