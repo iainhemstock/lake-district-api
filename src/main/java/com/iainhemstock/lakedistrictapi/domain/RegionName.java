a2183f665ef85c1ca9cf5d75b7ec43f625ae24c6
Unknown <iainhemstock@googlemail.com>
2020-12-23 17:17:14 +0000

    create RegionName class

---
+++ src/main/java/com/iainhemstock/lakedistrictapi/domain/RegionName.java
@@ -0,0 +1,13 @@
+package com.iainhemstock.lakedistrictapi.domain;
+
+public class RegionName {
+    private String value;
+
+    public RegionName(final String value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return value;
+    }
+}
