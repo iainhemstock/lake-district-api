8e86d1796704575fa5d75a66d4590fc30436ed9b
Iain Hemstock <iainhemstock@googlemail.com>
2020-12-24 16:56:37 +0000

    add test for mapping zero links to dto

--- src/test/unit/java/com/iainhemstock/lakedistrictapi/assemblers/LinksDTOAssemblerTests.java
+++ src/test/unit/java/com/iainhemstock/lakedistrictapi/assemblers/LinksDTOAssemblerTests.java
@@ -5,12 +5,15 @@ import com.iainhemstock.lakedistrictapi.domain.LinkRel;
 import com.iainhemstock.lakedistrictapi.domain.Links;
 import com.iainhemstock.lakedistrictapi.dtos.LinksDTO;
 import com.iainhemstock.lakedistrictapi.serviceinterfaces.LinksDTOAssembler;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import java.util.Map;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class LinksDTOAssemblerTests {
 
@@ -27,5 +30,12 @@ public class LinksDTOAssemblerTests {
             is(Map.of(
                 "self", "http://localhost:8080/api/v1/fells/NY123456",
                 "parent", "http://localhost:8080/api/v1/fells/NY987654")));
+    }
+
+    @Test
+    public void given_zero_links_when_mapping_then_links_map_is_empty() {
+        LinksDTOAssembler linksDTOAssembler = new LinksDTOAssemblerImpl();
+        LinksDTO linksDTO = linksDTOAssembler.toDTO(new Links());
+        assertTrue(linksDTO.getLinks().isEmpty());
     }
 }
