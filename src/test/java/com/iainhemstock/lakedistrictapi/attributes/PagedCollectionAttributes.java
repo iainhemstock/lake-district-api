001b7b17f88e0cbec03138ea82288b8dac0c691e
Iain Hemstock <iainhemstock@googlemail.com>
2020-12-29 19:12:44 +0000

    delete unused FIRST and LAST attributes

--- src/test/java/com/iainhemstock/lakedistrictapi/attributes/PagedCollectionAttributes.java
+++ src/test/java/com/iainhemstock/lakedistrictapi/attributes/PagedCollectionAttributes.java
@@ -2,11 +2,9 @@ package com.iainhemstock.lakedistrictapi.attributes;
 
 public enum PagedCollectionAttributes {
 
-    FIRST_HREF(LinksAttributes.FIRST_HREF.value()),
     PREV_HREF(LinksAttributes.PREV_HREF.value()),
     SELF_HREF(LinksAttributes.SELF_HREF.value()),
     NEXT_HREF(LinksAttributes.NEXT_HREF.value()),
-    LAST_HREF(LinksAttributes.LAST_HREF.value()),
     OFFSET("$.offset"),
     LIMIT("$.limit"),
     TOTAL_ITEMS("$.total_items");
