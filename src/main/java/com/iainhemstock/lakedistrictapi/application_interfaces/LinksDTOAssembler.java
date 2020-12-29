c81f57d87d8258bb2497d73e7c2aaa158c511e96
Iain Hemstock <iainhemstock@googlemail.com>
2020-12-29 18:55:04 +0000

    delete unused LinksDTOAssembler interface

--- src/main/java/com/iainhemstock/lakedistrictapi/application_interfaces/LinksDTOAssembler.java
+++
@@ -1,8 +0,0 @@
-package com.iainhemstock.lakedistrictapi.application_interfaces;
-
-import com.iainhemstock.lakedistrictapi.domain.Links;
-import com.iainhemstock.lakedistrictapi.infrastructure.spring_based_apps.spring_rest_api.dtos.LinksDTO;
-
-public interface LinksDTOAssembler {
-    LinksDTO toDTO(final Links links);
-}
