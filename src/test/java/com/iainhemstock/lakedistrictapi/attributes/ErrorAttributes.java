909e158c7435bf232e799210fbd934210ecb0b99
Iain Hemstock <iainhemstock@googlemail.com>
2020-12-29 19:11:15 +0000

    delete unused PATH attribute

--- src/test/java/com/iainhemstock/lakedistrictapi/attributes/ErrorAttributes.java
+++ src/test/java/com/iainhemstock/lakedistrictapi/attributes/ErrorAttributes.java
@@ -3,7 +3,6 @@ package com.iainhemstock.lakedistrictapi.attributes;
 public enum ErrorAttributes {
     STATUS("$.status"),
     MESSAGE("$.message"),
-    PATH("$.path"),
     TIMESTAMP("$.timestamp");
 
     private String value;
