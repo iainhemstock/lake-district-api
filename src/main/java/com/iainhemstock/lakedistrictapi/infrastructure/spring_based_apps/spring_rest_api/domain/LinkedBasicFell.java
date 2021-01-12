8db7cd8248b27be54373b7ec10a30baf080c4827
Iain Hemstock <iainhemstock@googlemail.com>
2021-01-12 22:16:53 +0000

    fix: correctly serialize LinkedBasicFell in response
    Before, a full LinkedFell (a subclass of LinkedBasicFell) was getting serialized in the response. By adding the @JsonSerialize(as=LinkedBasicFell.class) to the interface of LinkedBasicFell it ensures that Jackson serializes the interface only and not the subclass.

--- src/main/java/com/iainhemstock/lakedistrictapi/infrastructure/spring_based_apps/spring_rest_api/domain/LinkedBasicFell.java
+++ src/main/java/com/iainhemstock/lakedistrictapi/infrastructure/spring_based_apps/spring_rest_api/domain/LinkedBasicFell.java
@@ -1,9 +1,11 @@
 package com.iainhemstock.lakedistrictapi.infrastructure.spring_based_apps.spring_rest_api.domain;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.iainhemstock.lakedistrictapi.domain.FellName;
 
 import java.util.EnumMap;
 
+@JsonSerialize(as = LinkedBasicFell.class)
 public interface LinkedBasicFell {
     FellName getName();
     EnumMap<LinkRel, Link> getLinks();
